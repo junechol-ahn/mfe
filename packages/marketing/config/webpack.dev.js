59b30231ddfb140540fa9db0da23eedd2772e6b8
junechol ahn <junechol.ahn@gmail.com>
2026-02-25 22:32:30 +0900

    created container deploy script

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,7 +1,7 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json')
 
 const devConfig = {
   mode: 'development',
@@ -18,9 +18,6 @@ const devConfig = {
       exposes: {
         './MarketingApp': './src/bootstrap',
       },
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
     }),
   ],
 };
